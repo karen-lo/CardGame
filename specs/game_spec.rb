44eb031f4effc1b84a291aaf427e39147748a048
Karen Lo <lo.karen.y@gmail.com>
2018-10-23 01:15:30 -0700

    test for correct first player

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -38,7 +38,26 @@ describe Game do
   end
 
   describe ".play_game" do
-   context "given a game" do
+   
+   context "given a game of two players" do
+     before :context do
+       @deck = Deck.new
+       @num_players = 2
+       @game = Game.new(@deck, @num_players)
+       @game.play_game
+     end
+
+     it "passes out a hand to each player" do
+       expect(GameSpecHelper.check_player_hands(@game.players)).to be true
+     end
+
+     it "starts with the player with the lowest card" do
+       expect(GameSpecHelper.check_correct_first_player(@game.players)).to be true
+     end
+
+   end
+
+   context "given a game of three players" do
      before :context do
        @deck = Deck.new
        @num_players = 3
@@ -48,6 +67,27 @@ describe Game do
 
      it "passes out a hand to each player" do
        expect(GameSpecHelper.check_player_hands(@game.players)).to be true
+     end
+
+     it "starts with the player with three of clubs" do
+       expect(GameSpecHelper.check_correct_first_player(@game.players)).to be true
+     end
+   end
+   
+   context "given a game of four players" do
+     before :context do
+       @deck = Deck.new
+       @num_players = 4
+       @game = Game.new(@deck, @num_players)
+       @game.play_game
+     end
+
+     it "passes out a hand to each player" do
+       expect(GameSpecHelper.check_player_hands(@game.players)).to be true
+     end
+
+     it "starts with the player with three of clubs" do
+       expect(GameSpecHelper.check_correct_first_player(@game.players)).to be true
      end
    end
   end
--- specs/game_spec_helper.rb
+++ specs/game_spec_helper.rb
@@ -1,10 +1,41 @@
+# /specs/game_spec_helper.rb
+# Module of helper functions for Game specs
+
 module GameSpecHelper
+  THREE_OF_DIAMONDS = 2
+  SUIT_SIZE         = 13
+  DECK_SIZE         = 52
 
   def GameSpecHelper.check_player_hands(players)
     players.each do |player|
       return false if player.hand == nil
     end
     return true
+  end
+
+  def GameSpecHelper.check_correct_first_player(players)
+    if players.length < 2 || players.length > 4
+      raise 'Error: do not support this number of players'
+    end
+    
+    if players.length > 2
+      return players[0].hand.include?(Card.new(THREE_OF_DIAMONDS))
+    end
+    
+    smallest_card = THREE_OF_DIAMONDS
+    incr_value = 0
+    while smallest_card < DECK_SIZE
+      return true if players[0].hand.include?(Card.new(smallest_card))
+      return false if players[1].hand.include?(Card.new(smallest_card))
+      
+      if smallest_card + SUIT_SIZE > DECK_SIZE-1
+        incr_value += 1
+        smallest_card = THREE_OF_DIAMONDS + incr_value
+      else
+        smallest_card += SUIT_SIZE
+      end
+    end
+    return false
   end
 
 end
